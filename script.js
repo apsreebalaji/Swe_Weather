b4225847fae97c0fcbb10d14a29f9789753f5580
Sree Balaji A P <52956980+apsreebalaji@users.noreply.github.com>
2024-05-22 22:08:23 +0530

    Add functionality to change background image on respo nse

--- script.js
+++ script.js
@@ -1,124 +1,76 @@
 const container = document.querySelector('.container');
-
 const search = document.querySelector('.search-box button');
-
 const weatherBox = document.querySelector('.weather-box');
-
 const weatherDetails = document.querySelector('.weather-details');
-
 const error404 = document.querySelector('.not-found');
- 
 
-search.addEventListener('click',() =>{
-
-    // Enter your API Key in the APIKey variable
-
-    // You can use any weather api for the project
-
-    // Here we are using openweathermap's API which
-
-    // you can find in their website by searching 
-
-    // weather API.
-
+search.addEventListener('click', () => {
     const APIKey = '6e04847819a6f14e6cd0ebd509380e26';
     const city = document.getElementById('search-btn').value;
 
-    if(city==''){
-
-        return ;
-
-    }
- 
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
-
-     
-
-    if(json.cod == '404'){
-
-        container.style.height = '450px';
-
-        weatherBox.classList.remove('active');
-
-        weatherDetails.classList.remove('active');
-
-        error404.classList.add('active');
+    if (city === '') {
         return;
-
     }
- 
 
-    container.style.height = '560px';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.cod == '404') {
+                container.style.height = '450px';
+                weatherBox.classList.remove('active');
+                weatherDetails.classList.remove('active');
+                error404.classList.add('active');
+                return;
+            }
 
-    weatherBox.classList.add('active');
+            container.style.height = '560px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
 
-    weatherDetails.classList.add('active');
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
 
-    error404.classList.remove('active');
- 
+            let backgroundImage;
 
-    const image = document.querySelector('.weather-box img');
-
-    const temperature = document.querySelector('.weather-box .temperature');
-
-    const description = document.querySelector('.weather-box .description');
-
-    const humidity = document.querySelector('.weather-details .humidity span');
-
-    const wind = document.querySelector('.weather-details .wind span');
- 
-
-        switch(json.weather[0].main){
-
-            case 'Clear':
-                image.src = 'images/clear-new.png';
-
-                break;
-
-            case 'Rain':
-
-                image.src = 'images/rain-new.png';
-
-                break;
-
-            case 'Snow':
-
-                image.src = 'images/snow-new.png';
-
-                break;
-
-            case 'Clouds':
-
-                image.src = 'images/cloud-new.png';
-
-                break; 
-
-            case 'Mist':
-
-                image.src = 'images/mist-new.png';
-
-                break;                  
-
-            case 'Haze':
-
-                image.src = 'images/mist-new.png';
-
-                break;     
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear-new.png';
+                    backgroundImage = 'images/clear-new.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain-new.png';
+                    backgroundImage = 'images/rain-background.jpg';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow-new.png';
+                    backgroundImage = 'images/snow-background.jpg';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/cloud-new.png';
+                    backgroundImage = 'images/cloud-background.jpg';
+                    break;
+                case 'Mist':
+                    image.src = 'images/mist-new.png';
+                    backgroundImage = 'images/mist-new.png';
+                    break;
+                case 'Haze':
+                    image.src = 'images/mist-new.png';
+                    backgroundImage = 'images/haze-background.jpg';
+                    break;
                 default:
+                    image.src = 'images/clear-new.png';
+                    backgroundImage = 'images/default-background.jpg';
+            }
 
-                image.src = 'images/clear-new.png';
+            document.body.style.backgroundImage = `url('${backgroundImage}')`;
 
-        }
-
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-
-        description.innerHTML = `${json.weather[0].description}`;
-
-        humidity.innerHTML = `${json.main.humidity}%`;
-
-        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-    });
- 
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+        });
 });
